715b453a35dc87de037131833132ab21f3e88263
prince <princenandoliya@gmail.com>
2026-05-09 13:25:19 +0530

    update

--- 06_express_crud/app.js
+++ 06_express_crud/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import HttpError from "./middleware/HttpError.js";
 
 const app = express();
+app.use(express.json())
 
 const tasklist = [
     { id: 1, task: "error handling", message: "Implement custom error handling using HttpError" },
@@ -55,6 +56,42 @@ app.post("/addtask",(req,res,next)=>{
     res.status(201).json({success:true,message:"new task added successfully",newTask})
 
 })
+
+
+//delete
+
+app.delete("tasklist/:id",(req,res,next)=>{
+    const id = Number(res.params.id)
+
+    const index = tasklist.findIndex((t)=> t.id === id)
+
+    if(index === -1){
+        return next(new HttpError("requested route not found",404))
+    }
+
+    tasklist.splice(index,1)
+
+    res.status(200).json({
+        success:true,
+        message: "task data deleted successfully"
+    })
+})
+
+//err
+app.use((req,res,next)=>{
+    return next(new HttpError("requested route not found",404))
+})
+
+app.use((error,req,res,next)=>{
+    if(res.headersSent){
+        return next(error)
+    }
+    res.status(error.statusCode || 500).json({
+        message: error.message || "something want wrong please try again"
+    })
+})
+
+
 
 const port = 5000;
 
