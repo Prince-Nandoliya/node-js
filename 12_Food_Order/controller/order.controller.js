21c67735e71e381e76d998a7281995c27acf7d1f
prince <princenandoliya@gmail.com>
2026-08-16 15:19:07 +0530

    order controller code add

---
+++ 12_Food_Order/controller/order.controller.js
@@ -0,0 +1,14 @@
+import ordermodel from "../model/order.model.js";
+import HttpError from "../middleware/HttpError.js"
+
+
+const add = async(req,res,next) => {
+    try {
+        
+        const {CustomerName,Restaurant,FoodItems,totalAmount,DeliveryAddress,phone,orderStatus} = req.body
+
+
+    } catch (error) {
+        
+    }
+}
