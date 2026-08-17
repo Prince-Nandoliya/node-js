a2cc204da7699546cc31661a6bf356aa92bfa8e0
prince <princenandoliya@gmail.com>
2026-08-17 11:13:52 +0530

    order controller code add

--- 12_Food_Order/controller/order.controller.js
+++ 12_Food_Order/controller/order.controller.js
@@ -1,14 +1,43 @@
 import ordermodel from "../model/order.model.js";
 import HttpError from "../middleware/HttpError.js"
+import foodModel from "../model/Food.model.js";
 
 
-const add = async(req,res,next) => {
+const add = async (req, res, next) => {
     try {
-        
-        const {CustomerName,Restaurant,FoodItems,totalAmount,DeliveryAddress,phone,orderStatus} = req.body
+        const userId = req.user._id
+
+        const { Restaurant, FoodItems, DeliveryAddress, phone } = req.body
+
+        if (!FoodItems || FoodItems.length === 0) {
+            return next(new HttpError("At least one foodItems is required", 400))
+        }
+
+        let totalAmount = 0;
+
+        totalAmount += foodPrice * FoodItems.quantity
 
 
+        const newOrder = await ordermodel({
+            user: userId,
+            Restaurant,
+            FoodItems,
+            totalAmount,
+            DeliveryAddress,
+            phone
+        })
+
+        await newOrder.save()
+
+        res.status(201).json({
+            success: true,
+            message: "order placed successfully",
+            newOrder
+        })
     } catch (error) {
-        
+        next(new HttpError(error.message, 500))
+
     }
 }
+
+export default { add }
